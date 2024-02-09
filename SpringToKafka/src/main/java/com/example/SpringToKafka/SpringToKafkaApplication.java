3c0a65860aa23a612ffe742d715e52f3e343e157
lehar <lehar@gmail.com>
2024-02-09 15:11:03 +0530

    spring to kafka

---
+++ SpringToKafka/src/main/java/com/example/SpringToKafka/SpringToKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringToKafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringToKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringToKafkaApplication.class, args);
+	}
+
+}
